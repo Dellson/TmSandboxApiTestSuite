90b263884f85535755583e3e5aa25022cf356146
Paweł Dela <pwdela@gmail.com>
2022-05-11 19:42:33 +0200

    Create REST API test solution
    * Initial set up of the solution
    
    * Added the first test alongside with model
    
    * Added more tests
    
    * Added flexibility with configuration
    
    * Fixed wrong assertion in a test
    
    * Added an additional test
    
    Co-authored-by: Paweł Dela <pawel.dela@amberteam.pl>

---
+++ TmSandboxApiTestSuite/TmSandboxApiTestSuite/Configuration/AppConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace TmSandboxApiTestSuite.Configuration
+{
+    internal class AppConfiguration
+    {
+        private readonly IConfiguration _config;
+
+        public string BaseUrl
+        {
+            get
+            {
+                return _config.GetSection("AppSettings").GetSection("BaseUrl").Value;
+            }
+        }
+
+        public AppConfiguration()
+        {
+            _config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.local.json")
+                .Build();
+        }
+    }
+}
---
+++ TmSandboxApiTestSuite/TmSandboxApiTestSuite/Tests/CategoryDetailsTests.cs
@@ -0,0 +1,79 @@
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using TmSandboxApiTestSuite.Configuration;
+using TmSandboxApiTestSuite.Model;
+
+namespace TmSandboxApiTestSuite.Tests
+{
+    [TestClass]
+    public class CategoryDetailsTests
+    {
+        private const string _path = "v1/Categories/6327/Details.json?catalogue=false";
+        private RestClient _client;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            var config = new AppConfiguration();
+            _client = new RestClient(config.BaseUrl);
+        }
+
+        [TestMethod]
+        async public Task GetCategory6327Details_WhenCatalogueIsFalse_ReturnsCorrectName()
+        {
+            // Arrange
+            RestRequest request = new RestRequest(_path);
+
+            // Act
+            RestResponse<CategoryDetails> response = await _client.ExecuteGetAsync<CategoryDetails>(request);
+
+            // Assert
+            response.Data.Name
+                .Should().Be("Carbon credits");
+        }
+
+        [TestMethod]
+        async public Task GetCategory6327Details_WhenCatalogueIsFalse_ReturnsCorrectCanRelistValue()
+        {
+            // Arrange
+            RestRequest request = new RestRequest(_path);
+
+            // Act
+            RestResponse<CategoryDetails> response = await _client.ExecuteGetAsync<CategoryDetails>(request);
+
+            // Assert
+            response.Data.CanRelist
+               .Should().BeTrue();
+        }
+
+        [TestMethod]
+        async public Task GetCategory6327Details_WhenCatalogueIsFalse_ReturnsCorrectDescriptionForDistinctPromotionsGallery()
+        {
+            // Arrange
+            RestRequest request = new RestRequest(_path);
+
+            // Act
+            RestResponse<CategoryDetails> response = await _client.ExecuteGetAsync<CategoryDetails>(request);
+
+            // Assert
+            response.Data.Promotions.Where(p => p.Name == "Gallery").Should().HaveCount(1);
+            response.Data.Promotions.Single(p => p.Name == "Gallery").Description
+                .Should().Contain("Good position in category");
+        }
+
+        [TestMethod]
+        async public Task GetCategory6327Details_WhenCatalogueIsFalse_ReturnsCorrectDescriptionForEachPromotionsGallery()
+        {
+            // Arrange
+            RestRequest request = new RestRequest(_path);
+
+            // Act
+            RestResponse<CategoryDetails> response = await _client.ExecuteGetAsync<CategoryDetails>(request);
+
+            // Assert
+            response.Data.Promotions.Where(p => p.Name == "Gallery")
+                .Should().OnlyContain(p => p.Description == "Good position in category");
+        }
+    }
+}
